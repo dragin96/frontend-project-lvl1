931c9ab7f5c217ed8bd609b3ee40835c407b4f38
dragin96 <alexxx55509@mail.ru>
2021-02-04 10:35:28 +0300

    feat: add checkers for code

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
+import { run } from '../src/cli.mjs';
+
 const welcomeGame = () => {
-    console.log("Welcome to the Brain Games!")
-}
+  console.log('Welcome to the Brain Games!');
+};
 
 welcomeGame();
+run();
