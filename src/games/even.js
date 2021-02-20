76a48afd5f9fe52f5d119460c17b5ffc694f8670
dragin96 <alexxx55509@mail.ru>
2021-02-20 22:41:34 +0300

    refactors: fix review

--- src/games/even.js
+++ src/games/even.js
@@ -3,14 +3,12 @@ import initGame from '../index.js';
 
 const isEvenNumber = (number) => number % 2 === 0;
 
-const evenGame = {
-  description: 'Answer "yes" if the number is even, otherwise answer "no".',
-  getQuestionWithAnswer() {
-    const maxRandom = 1000;
-    const question = getRandomInt(0, maxRandom);
-    const answer = isEvenNumber(question) ? 'yes' : 'no';
-    return { question, answer };
-  },
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const getQuestionWithAnswer = () => {
+  const maxRandom = 1000;
+  const question = getRandomInt(0, maxRandom);
+  const answer = isEvenNumber(question) ? 'yes' : 'no';
+  return { question, answer };
 };
 
-export default initGame(evenGame.description, evenGame.getQuestionWithAnswer);
+export default initGame(description, getQuestionWithAnswer);
